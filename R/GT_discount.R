2d4a796ddad4f1b448898a30d152df65cded00ba
Omayma Said <OmaymaS@users.noreply.github.com>
2017-03-12 18:00:52 +0200

    add GT_discount

---
+++ R/GT_discount.R
@@ -0,0 +1,27 @@
+#' Get Good Turing discount from ngram frequency table
+#'
+#' @param df data.frame
+#' @param freq name of freq column
+#' @param mx max freq to consider for GT discount
+#'
+#' @export
+
+GT_discount <- function(df,freq=r,mx=6){
+        df %>%
+                group_by(r) %>%
+                summarise(Nr=n()) %>%
+                filter(r<=mx) %>%
+                arrange(r) %>%
+                mutate(GT_discount=((r+1)/r)*(lead(Nr,1)/Nr)) %>%
+                head(.,mx-1)
+
+}
+
+
+# freq_of_freq2 <- function(dt,freq=r){
+#         dt <- dt[,list(Nr=.N),by=r]
+#         dt <- dt[r<=6]
+#         dt <- dt[order(r)]
+#
+#         return(dt)
+# }
