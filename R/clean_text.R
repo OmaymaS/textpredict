3e93328e59a923c7bf0c740a9cba13660a1f09d7
Omayma Said <OmaymaS@users.noreply.github.com>
2017-03-12 18:00:24 +0200

    add export and remove print debugging statements

--- R/clean_text.R
+++ R/clean_text.R
@@ -1,6 +1,6 @@
 #' Clean raw text
 #'
-
+#' @export
 clean_text <- function(x, profanity=profanity){
         y <- x %>%
                 stri_trans_general(.,id="latin-ascii") %>%
@@ -11,15 +11,11 @@ clean_text <- function(x, profanity=profanity){
                 tolower() %>%
                 removeWords(.,profanity) %>%
                 gsub("\\br(t)\\b|\\blo{1,}l\\b|\\bz{2,}\\b", " ", .) %>%
-                print() %>%
                 gsub("http[[:alnum:]]*|www[[:alnum:]]*", " ", .) %>%
-                print() %>%
                 # gsub("\\b[b-hj-z]\\b", " ", .) %>%
                 gsub("(?<!\\w|\')[b-hj-z](?!\\w|\')", " ",.,perl=T) %>%
-                print() %>%
                 stripWhitespace() %>%
                 trimws()
-
 
         return(y)
 }
