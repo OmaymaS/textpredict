61a2e2fa8ecc75a220d019acd330518aa75b37b4
Omayma Said <OmaymaS@users.noreply.github.com>
2017-03-12 23:10:51 +0200

    add script to download swiftkey_dataset

---
+++ data-raw/get_swiftkey_dataset.R
@@ -0,0 +1,18 @@
+sk_url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+
+if(!file.exists("data-raw/swiftkey_dataset/swiftkey.zip"))
+{
+        download.file(sk_url, destfile="data-raw/swiftkey_dataset/swiftkey.zip")
+}
+
+if(!file.exists("./swiftkey_dataset/swiftkey_unzipped"))
+{
+        dir.create("./swiftkey_dataset/swiftkey_unzipped")
+}
+
+
+if(length(list.files("./swiftkey_dataset/swiftkey_unzipped"))==0)
+{
+        unzip("swiftkey.zip",exdir="./swiftkey_dataset/swiftkey_unzipped")
+}
+
