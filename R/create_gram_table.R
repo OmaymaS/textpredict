dfcdc21125274433416c691cc4d5096f2d7f0220
Omayma Said <OmaymaS@users.noreply.github.com>
2017-03-12 16:41:32 +0200

    add data.frame implementation and add @import data.table with data.table impementation

--- R/create_gram_table.R
+++ R/create_gram_table.R
@@ -1,12 +1,14 @@
+#' Create n-gram tables with frequency - data.table implementation
+
 create_gram_table <- function(x,n){
 
-        # df <- data.table(txt=x)
-
-        g <- unnest_tokens(data.table(txt=x),
+        g <- unnest_tokens(data.frame(txt=x),
                            ngram, txt,
                            token = "ngrams", n = n)
         g <- data.table(g)
+        print(class(g))
         g <- g[,.N,by=ngram]
+
         g <- g[order(N,decreasing = T)]
 
         return(g)
