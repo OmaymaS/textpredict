fadcc2fa17a13fa60e215636b9f9559085c161f6
Omayma Said <OmaymaS@users.noreply.github.com>
2017-06-11 01:01:41 +0200

    comment 2nd implementation and leave the original

--- R/create_gram_table.R
+++ R/create_gram_table.R
@@ -1,15 +1,15 @@
-#' create_gram_table: Create n-gram tables with frequency - dataframe implementation
+#' create_gram_table: Create n-gram tables with frequency
 #'
 #' @export
 #'
-create_gram_table <- function(x,n){
+create_gram_table <- function(x, input_col, n){
 
-        g <- unnest_tokens(data.frame(txt=x),
-                           ngram, txt,
-                           token = "ngrams", n = n) %>%
-                group_by(ngram) %>%
-                summarise(N=n()) %>%
-                arrange(desc(N))
+        g <- tidytext::unnest_tokens(x, ngram, input_col,
+                                     token = "ngrams", n = n)
+        # %>%
+        #         group_by(ngram) %>%
+        #         summarise(N=n()) %>%
+        #         arrange(desc(N))
 
         return(g)
 }
@@ -17,18 +17,17 @@ create_gram_table <- function(x,n){
 
 #' create_gram_table2: Create n-gram tables with frequency - data.table implementation
 #'
-#' @export
-#' @import data.table
-#'
 
-create_gram_table2 <- function(x,n){
 
-        g <- unnest_tokens(data.frame(txt=x),
-                           ngram, txt,
-                           token = "ngrams", n = n)
-        g <- data.table(g)
-        g <- g[,list(r=.N),by=ngram]
-        g <- g[order(r,decreasing = T)]
 
-        return(g)
-}
+# create_gram_table2 <- function(x,n){
+#
+#         g <- unnest_tokens(data.frame(txt=x),
+#                            ngram, txt,
+#                            token = "ngrams", n = n)
+#         g <- data.table(g)
+#         g <- g[,list(r=.N),by=ngram]
+#         g <- g[order(r,decreasing = T)]
+#
+#         return(g)
+# }
